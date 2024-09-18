3d2a8b16ce0dda448d4dc6eb6a08ced01d5baad1
MelEdur <meleduardovi@gmail.com>
2024-09-17 22:14:13 -0300

    init proyect

---
+++ tp-sistemas-distribuidos-grpc/src/main/java/com/unla/tp/TpSistemasDistribuidosGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.unla.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSistemasDistribuidosGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSistemasDistribuidosGrpcApplication.class, args);
+	}
+
+}
---
+++ tp-sistemas-distribuidos-grpc/src/test/java/com/unla/tp/TpSistemasDistribuidosGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSistemasDistribuidosGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
