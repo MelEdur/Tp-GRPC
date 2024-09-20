7827ca71881490a12a130d18274b8316118e8905
Kevin <31149673+kevinunla@users.noreply.github.com>
2024-09-20 04:03:44 -0300

    Actualizacion serviceProto cliente

--- Servidor GRPC/tp-sistemas-distribuidos-grpc/src/main/java/com/unla/tp/repository/IProductoRepository.java
+++ Servidor GRPC/tp-sistemas-distribuidos-grpc/src/main/java/com/unla/tp/repository/IProductoRepository.java
@@ -3,11 +3,9 @@ package com.unla.tp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-import java.util.Optional;
 import com.unla.tp.entity.ProductoEntity;
 
 @Repository
 public interface IProductoRepository extends JpaRepository<ProductoEntity, Integer>{
-
     List<ProductoEntity> findByCodigoProductoContainingAndNombreProductoContainingAndTalleContainingAndColorContaining(String codigoProducto, String nombreProducto, String talle, String color);
 }
