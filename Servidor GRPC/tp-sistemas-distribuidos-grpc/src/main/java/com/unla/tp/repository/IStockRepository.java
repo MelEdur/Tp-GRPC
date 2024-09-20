3ec32c659554221db4489fdb14c33f8182030ad8
Kevin <31149673+kevinunla@users.noreply.github.com>
2024-09-20 00:39:41 -0300

    Agregado ProductoService Cambios Stock+ProductoRepository+ServiceProto
    AgregarProducto Funciona pero en las pruebas postman devuelve error.
    ModificarProducto Funcionando sin fallas
    TraerProductos Funcionando sin fallas
    EliminarProducto Funcionando sin fallas(Baja Logica en tabla producto + tabla stock)
    TraerProductosPorFiltro Funcionando sin fallas

--- Servidor GRPC/tp-sistemas-distribuidos-grpc/src/main/java/com/unla/tp/repository/IStockRepository.java
+++ Servidor GRPC/tp-sistemas-distribuidos-grpc/src/main/java/com/unla/tp/repository/IStockRepository.java
@@ -6,12 +6,13 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.unla.tp.entity.ProductoEntity;
 import com.unla.tp.entity.StockEntity;
 
 @Repository
 public interface IStockRepository extends JpaRepository<StockEntity, Integer>{
     Optional<List<StockEntity>> findByProductoId(Integer productoId);
     Optional<List<StockEntity>> findByTiendaId(Integer tiendaId);
-
     Optional<List<StockEntity>> findByProductoIdAndTiendaId(Integer productoId, Integer tiendaId);
+    List<StockEntity> findByProducto(ProductoEntity producto);
 }
