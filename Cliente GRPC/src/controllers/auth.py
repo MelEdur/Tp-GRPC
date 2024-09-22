b51c9999900dc550f7175e6c925f4f6409405208
MelEdur <meleduardovi@gmail.com>
2024-09-22 13:44:44 -0300

    Añadido cliente de login

---
+++ Cliente GRPC/src/controllers/auth.py
@@ -0,0 +1,21 @@
+import os, sys
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(CURRENT_DIR,'..','clients'))
+
+from flask import Blueprint, request
+from flask_cors import cross_origin
+from google.protobuf.json_format import MessageToJson
+
+from clients.AuthCliente import AuthCliente
+
+auth_blueprint = Blueprint('auth',__name__)
+
+cliente = AuthCliente()
+
+@auth_blueprint.route('/login',methods=['POST'])
+@cross_origin()
+def login():
+
+    data = request.get_json()
+    result = cliente.login(data)
+    return MessageToJson(result)
