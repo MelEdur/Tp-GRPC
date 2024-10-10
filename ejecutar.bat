11356a6d5ffe74311e6cb0517916935f1ff78fea
MelEdur <meleduardovi@gmail.com>
2024-10-09 22:02:59 -0300

    Actualizado vista actualizar stock y ejecutar.bat

--- ejecutar.bat
+++ ejecutar.bat
@@ -1,3 +1,5 @@
 start cmd /k"cd Servidor GRPC/tp-sistemas-distribuidos-grpc && mvn spring-boot:run"
+start cmd /k"cd Proveedor/proveedor && mvn spring-boot:run"
 start cmd /k"cd Cliente GRPC/src && python app.py"
 start cmd /k"cd Vista && python -m http.server 8000"
+start cmd /k"cd Vista Proveedor && python -m http.server 8001"
